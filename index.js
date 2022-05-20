f5a27f907cf20d0c8048db5aed0e10ef1bc7f5c0
devnoman01 <devnoman01@gmail.com>
2022-05-21 02:38:30 +0600

    checking changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 const { MongoClient, ServerApiVersion } = require("mongodb");
